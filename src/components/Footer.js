f4673b4302bf09fb82c7a32811124bde67d16a81
nhatdang200210 <149038599+nhatdang200210@users.noreply.github.com>
2023-10-27 18:17:27 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import foot from'../imageNLCS/footer1.png';
 export default function Footer() {
   return (
     <footer className="footer">
+    <p> nhat </p>
         <img alt='' src={foot} className="footer-img" />
     </footer>
   )
