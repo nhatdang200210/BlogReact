0e7616280d4bb26afeb62b01e0dac7a89338fb19
Server <nd7433@gmail.com>
2023-11-17 11:24:58 +0700

    xem thêm and thu gọn comment

---
+++ src/components/Post/CommentItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import moment from 'moment';
+import { IconButton, Typography } from '@material-ui/core';
+import Delete from '@material-ui/icons/Delete';
+
+export default function CommentItem({ comment, onDeleteComment }) {
+  const handleDelete = () => {
+    onDeleteComment(comment._id);
+  };
+
+  return (
+    <div style={{ borderBottom: "1px solid #ddd" }}>
+      <div style={{ display: "flex" }}>
+        <Typography variant="h6" style={{ fontSize: "13px", marginRight: "20px" }}>
+          {comment.author} 
+        <IconButton onClick={handleDelete} style={{marginBottom: "0px", color:"brown"}}>
+             <Delete />
+        </IconButton>
+        </Typography>
+      </div>
+      <div style={{ display: "flex" }}>
+        <Typography style={{ fontSize: "8px" }}>
+          {moment(comment.createdAt).format("YYYY-MM-DD")}
+        </Typography>
+      </div>
+      <Typography style={{ fontSize: "13px" }} className="post-content">
+          {comment.content}
+        </Typography>
+        
+    </div>
+  );
+}
